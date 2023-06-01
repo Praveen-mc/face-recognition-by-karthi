63d4c9ba167b5df30962a5d82c06bcaf9d9b6b6f
Praveen-mc <praveensb2002@gmail.com>
2023-06-01 11:22:54 +0530

    FileView.py added

---
+++ FileView.py
@@ -0,0 +1,20 @@
+import PySimpleGUI as sg
+import PIL.Image
+import io
+import base64
+def convert_to_bytes(file_or_bytes, resize=None):
+   img = PIL.Image.open(file_or_bytes)
+   with io.BytesIO() as bio:
+      img.save(bio, format="PNG")
+      del img
+      return bio.getvalue()
+def open() :
+   imgdata = convert_to_bytes("C:\\Users\\LENOVO\\Desktop\\sample\\face-recognition-vault\\have a good day project.png")
+   layout = [[sg.Image(key='-IMAGE-', size=(1000,1000),data=imgdata)]]
+   window = sg.Window('PIL based Image Viewer', layout,resizable=True)
+   
+while True:
+   event, values = window.read()
+   if event == sg.WIN_CLOSED:
+      break
+window.close()
