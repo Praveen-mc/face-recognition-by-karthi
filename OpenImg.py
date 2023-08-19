34d19e93462b10a7adffe416facfc00dedea3bf9
Praveen <89517997+Praveen-mc@users.noreply.github.com>
2023-08-19 17:16:09 +0530

    Update OpenImg.py

--- OpenImg.py
+++ OpenImg.py
@@ -7,6 +7,3 @@ def open_image(file_path):
     except Exception as e:
         print("An error occurred:", e)
 
-if __name__ == "__main__":
-    image_path = "C:\\Users\\LENOVO\\Desktop\\sample\\face-recognition-vault\\have a good day project.png"  # Replace with the actual path to your image file
-    open_image(image_path)
